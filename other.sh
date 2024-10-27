c69938f64f92de82f29fe0cdb2db87701e3aa04f
shiinarimo <58881591+shiinarimo@users.noreply.github.com>
2024-10-27 17:00:20 +0900

    Update other.sh

--- other.sh
+++ other.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/MetaCubeX/meta-rules-dat"
 BRANCH="meta"
 TARGET_DIR="geo/geosite"
 DEST_DIR="processed-files" # 替换为实际路径
-
+mkdir -p processed-files
 # 克隆特定文件夹
 git clone --depth 1 --branch $BRANCH $REPO_URL temp_repo
 cd temp_repo/$TARGET_DIR || exit
