ef0442e367514e58a6d8d2e15dce2d4049e12270
shiinarimo <58881591+shiinarimo@users.noreply.github.com>
2024-10-27 17:05:57 +0900

    Update other.sh

--- other.sh
+++ other.sh
@@ -5,11 +5,12 @@ REPO_URL="https://github.com/MetaCubeX/meta-rules-dat"
 BRANCH="meta"
 TARGET_DIR="geo/geosite"
 DEST_DIR="processed-files" # 替换为实际路径
-mkdir -p processed-files
+
 # 克隆特定文件夹
 git clone --depth 1 --branch $BRANCH $REPO_URL temp_repo
 cd temp_repo/$TARGET_DIR || exit
 
+mkdir -p processed-files
 # 保留扩展名为 .list 的文件，删除文件内的 "+" 字符
 for file in *.list; do
     sed 's/+//g' "$file" > "$DEST_DIR/$file"
